2c04368ea08a73f56ad5de6d372ce36d59498a84
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-02-27 16:28:13 +0530

    added

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, World! name :Nisha roll:109 location: Pune")
+    return HttpResponse("Hello, World! name :Manjusha roll:101 location: Pune")
